582e2cc0a13b1ea98e719f03b6c780a41567df91
Chon Lok Lei <chonloklei@gmail.com>
2021-04-30 12:49:03 +0800

    Add options to train-d0.py

--- train-d0.py
+++ train-d0.py
@@ -14,12 +14,11 @@ import pints
 
 import torch
 import torch.nn as nn
-import torch.optim as optim
-from torch.optim.lr_scheduler import StepLR
 
 parser = argparse.ArgumentParser('IKr discrepancy fit with the candidate model.')
 parser.add_argument('--method', type=str, choices=['dopri5', 'adams'], default='dopri5')
 parser.add_argument('--debug', action='store_true')
+parser.add_argument('--myokit', action='store_true', help='Use Myokit for speed up.')
 args = parser.parse_args()
 
 from torchdiffeq import odeint
@@ -48,16 +47,12 @@ raw_data1 = np.loadtxt('data/pr3-steady-activation-cell-5.csv', delimiter=',', s
 raw_data2 = np.loadtxt('data/pr5-deactivation-cell-5.csv', delimiter=',', skiprows=1)
 time1 = raw_data1[:, 0]
 time1_torch = torch.from_numpy(raw_data1[:, 0]).to(device)
-#current1 = raw_data1[:, 1]
 voltage1 = raw_data1[:, 2]
 time2 = raw_data2[:, 0]
 time2_torch = torch.from_numpy(raw_data2[:, 0]).to(device)
-#current2 = raw_data2[:, 1]
 voltage2 = raw_data2[:, 2]
 
 
-true_y0s = [torch.tensor([[1., 0.]]).to(device),  # what you get after holding at +40mV
-            torch.tensor([[0., 1.]]).to(device)]  # (roughly) what you get after holding at -80mV
 gt_true_y0s = [torch.tensor([[0., 0., 1., 0., 0., 0.]]).to(device),  # what you get after holding at +40mV
                torch.tensor([[0., 1., 0., 0., 0., 0.]]).to(device)]  # (roughly) what you get after holding at -80mV
 
@@ -69,26 +64,6 @@ def makedirs(dirname):
         os.makedirs(dirname)
 
 makedirs('d0')
-
-
-#
-# Timer
-#
-import signal
-from contextlib import contextmanager
-
-class TimeoutException(Exception): pass
-
-@contextmanager
-def time_limit(seconds):
-    def signal_handler(signum, frame):
-        raise TimeoutException('Simulation time out.')
-    signal.signal(signal.SIGALRM, signal_handler)
-    signal.alarm(seconds)
-    try:
-        yield
-    finally:
-        signal.alarm(0)
 
 
 #
@@ -149,133 +124,273 @@ class Lambda(nn.Module):
 
         return torch.stack([dc1dt[0], dc2dt[0], didt[0], dic1dt[0], dic2dt[0], dodt[0]])
 
+if args.myokit:
+    try:
+        import myokit
+    except:
+        pass # TODO
 
-class ODEFunc(nn.Module):
+    #
+    # Time out handler
+    #
+    class Timeout(myokit.ProgressReporter):
+        """
+        A :class:`myokit.ProgressReporter` that halts the simulation after
+        ``max_time`` seconds.
+        """
+        def __init__(self, max_time):
+            self.max_time = float(max_time)
+        def enter(self, msg=None):
+            self.b = myokit.Benchmarker()
+        def exit(self):
+            pass
+        def update(self, progress):
+            return self.b.time() < self.max_time
 
-    def __init__(self):
-        super(ODEFunc, self).__init__()
 
-        self.p1 = 1.13e-4
-        self.p2 = 7.45e-2
-        self.p3 = 3.60e-5
-        self.p4 = 4.49e-2
-        # Best of 10 fits (M10) for data herg25oc1 cell B06 (seed 542811797)
-        self.p5 = 9.62243079990877703e+01 * 1e-3
-        self.p6 = 2.26404683824047979e+01 * 1e-3
-        self.p7 = 8.00924780462999131e+00 * 1e-3
-        self.p8 = 2.43749808069009823e+01 * 1e-3
+    class Model(pints.ForwardModel):
+        """
+        # A voltage clamp (VC) model linking Myokit and Pints ForwardModel.
+        """
 
-        self.unity = torch.tensor([1]).to(device)
+        def __init__(self, model_file='ikr.mmt', max_evaluation_time=60):
+            """
+            # model_file: mmt model file for myokit; main units: mV, ms, pA.
+            # max_evaluation_time: maximum time (in second) allowed for one
+            #                      simulate() call.
+            """
+            self._model = myokit.load_model(model_file)
+            self._model_file = model_file
+            self._model_file_name = os.path.basename(model_file)
+            self._vhold = -80
 
-    def set_parameters(self, x):
-        #self.p1, self.p2, self.p3, self.p4, self.p5, self.p6, self.p7, self.p8 = x
-        self.p1, self.p2, self.p3, self.p4 = x
+            # maximum time allowed
+            self.max_evaluation_time = max_evaluation_time
 
-    def set_fixed_form_voltage_protocol(self, t, v):
-        # Regular time point voltage protocol time series
-        self._t_regular = t
-        self._v_regular = v
-        self.__v = interp1d(t, v)
+            # Create simulation protocol
+            self.simulation = myokit.Simulation(self._model)
+            self.simulation.set_tolerance(1e-6, 1e-8)
+            # self.simulation.set_max_step_size(1e-2)  # ms
 
-    def _v(self, t):
-        #return torch.from_numpy(np.interp([t.cpu().detach().numpy()], self._t_regular,
-        #                                  self._v_regular))
-        #return self.__v([t.cpu().detach().numpy()])
-        return torch.from_numpy(self.__v([t.cpu().detach().numpy()]))
+            # Init states
+            self.init_state = self.simulation.state()
 
-    def voltage(self, t):
-        # Return voltage
-        return self._v(t).numpy()
+        def n_parameters(self):
+            return 4
 
-    def forward(self, t, y):
-        a, r = torch.unbind(y, dim=1)
+        def set_init_state(self, v):
+            self.init_state = v
 
+        def set_voltage_protocol(self, p, prt_mask=None):
+            # Assume protocol p is
+            # [step_1_voltage, step_1_duration, step_2_voltage, ...]
+            # prt_mask: (numpy) mask function that remove part of the measurement;
+            #           can be used as a capacitive filter, or to make the fitting
+            #           harder
+            protocol = myokit.Protocol()
+            duration = 0
+            for i in range(len(p) // 2):
+                protocol.add_step(p[2 * i], p[2 * i + 1])
+                duration += p[2 * i + 1]
+            self.simulation.set_protocol(protocol)
+            del(protocol)
+            self.prt_mask = prt_mask
+
+        def set_fixed_form_voltage_protocol(self, t, v, prt_mask=None):
+            # v, t: voltage, time to be set in ms, mV
+            # prt_mask: (numpy) mask function that remove part of the measurement;
+            #           can be used as a capacitive filter, or to make the fitting
+            #           harder
+            self.simulation.set_fixed_form_protocol(
+                t, v  # ms, mV
+            )
+            self.prt_mask = prt_mask
+
+        def simulate(self, parameters, times):
+            # simulate() method for Pints
+
+            p1, p2, p3, p4 = parameters
+            self.simulation.set_constant('ikr.p1', p1)
+            self.simulation.set_constant('ikr.p2', p2)
+            self.simulation.set_constant('ikr.p3', p3)
+            self.simulation.set_constant('ikr.p4', p4)
+
+            # Reset to ensure each simulate has same init condition
+            self.simulation.reset()
+            self.simulation.set_state(self.init_state)
+
+            # Run!
+            try:
+                p = Timeout(self.max_evaluation_time)
+                d = self.simulation.run(np.max(times)+1e-3,
+                    log_times=times,
+                    log=['ikr.IKr'],
+                    progress=p,
+                    ).npview()
+                del(p)
+            except (myokit.SimulationError, myokit.SimulationCancelledError):
+                return np.full(times.shape, float('inf'))
+
+            # Apply capacitance filter and return
+            if self.prt_mask is not None:
+                fcap = np.zeros(times.shape)
+                fcap[self.prt_mask] = 1
+                d['ikr.IKr'] = d['ikr.IKr'] * self.fcap
+
+            return d['ikr.IKr']
+
+else:  # use torchdiffeq odeint
+
+    #
+    # Timer
+    #
+    import signal
+    from contextlib import contextmanager
+
+    class TimeoutException(Exception): pass
+
+    @contextmanager
+    def time_limit(seconds):
+        def signal_handler(signum, frame):
+            raise TimeoutException('Simulation time out.')
+        signal.signal(signal.SIGALRM, signal_handler)
+        signal.alarm(seconds)
         try:
-            v = self._v(t).to(device)
-        except ValueError:
-            v = torch.tensor([-80]).to(device)
-
-        k1 = self.p1 * torch.exp(self.p2 * v)
-        k2 = self.p3 * torch.exp(-self.p4 * v)
-        k3 = self.p5 * torch.exp(self.p6 * v)
-        k4 = self.p7 * torch.exp(-self.p8 * v)
-
-        dadt = k1 * (self.unity - a) - k2 * a
-        drdt = -k3 * r + k4 * (self.unity - r)
-
-        return torch.stack([dadt[0], drdt[0]]).reshape(1, -1)
+            yield
+        finally:
+            signal.alarm(0)
 
 
-class Model(pints.ForwardModel):
-    def __init__(self, ode_func=None):
-        super(Model, self).__init__()
-        if ode_func is None:
-            self._ode = ODEFunc().to(device)
-        else:
-            self._ode = ode_func
-        self.set_y0()
-        self.set_voltage_protocol_batches()
-        self.set_discontinous()
+    class ODEFunc(nn.Module):
 
-    def n_parameters(self):
-        #return 9
-        return 4
+        def __init__(self):
+            super(ODEFunc, self).__init__()
 
-    def n_outputs(self):
-        if self._ps is not None:
-            return len(self._ps)
-        else:
-            return 1
+            self.p1 = 1.13e-4
+            self.p2 = 7.45e-2
+            self.p3 = 3.60e-5
+            self.p4 = 4.49e-2
+            # Best of 10 fits (M10) for data herg25oc1 cell B06 (seed 542811797)
+            self.p5 = 9.62243079990877703e+01 * 1e-3
+            self.p6 = 2.26404683824047979e+01 * 1e-3
+            self.p7 = 8.00924780462999131e+00 * 1e-3
+            self.p8 = 2.43749808069009823e+01 * 1e-3
 
-    def set_fixed_form_voltage_protocol(self, t, v):
-        self._ode.set_fixed_form_voltage_protocol(t, v)
+            self.unity = torch.tensor([1]).to(device)
 
-    def set_voltage_protocol_batches(self, ps=None):
-        "ps: list of voltage time series [times, voltages]"
-        self._ps = ps
+        def set_parameters(self, x):
+            #self.p1, self.p2, self.p3, self.p4, self.p5, self.p6, self.p7, self.p8 = x
+            self.p1, self.p2, self.p3, self.p4 = x
 
-    def set_y0(self, y0=np.asarray([0, 1])):
-        self._y0 = y0.reshape(1, -1)
-        self._y0_torch = torch.from_numpy(self._y0).float()
+        def set_fixed_form_voltage_protocol(self, t, v):
+            # Regular time point voltage protocol time series
+            self._t_regular = t
+            self._v_regular = v
+            self.__v = interp1d(t, v)
 
-    def set_discontinous(self, discontn=None):
-        if discontn is not None:
-            self.discontn = torch.from_numpy(discontn)
-        else:
-            self.discontn = discontn
+        def _v(self, t):
+            #return torch.from_numpy(np.interp([t.cpu().detach().numpy()], self._t_regular,
+            #                                  self._v_regular))
+            #return self.__v([t.cpu().detach().numpy()])
+            return torch.from_numpy(self.__v([t.cpu().detach().numpy()]))
 
-    def simulate(self, x, t):
-        "Pints's forward simulation, x parameters, t time series"
-        t = torch.from_numpy(np.copy(t))
-        #g = x[0]
-        g = 1
-        #self._ode.set_parameters(x[1:])
-        self._ode.set_parameters(x[:])
-        if self._ps is not None:
-            out = []
-            for p in self._ps:
-                self.set_fixed_form_voltage_protocol(p[:, 0], p[:, 1])
+        def voltage(self, t):
+            # Return voltage
+            return self._v(t).numpy()
+
+        def forward(self, t, y):
+            a, r = torch.unbind(y, dim=1)
+
+            try:
+                v = self._v(t).to(device)
+            except ValueError:
+                v = torch.tensor([-80]).to(device)
+
+            k1 = self.p1 * torch.exp(self.p2 * v)
+            k2 = self.p3 * torch.exp(-self.p4 * v)
+            k3 = self.p5 * torch.exp(self.p6 * v)
+            k4 = self.p7 * torch.exp(-self.p8 * v)
+
+            dadt = k1 * (self.unity - a) - k2 * a
+            drdt = -k3 * r + k4 * (self.unity - r)
+
+            return torch.stack([dadt[0], drdt[0]]).reshape(1, -1)
+
+
+    class Model(pints.ForwardModel):
+        def __init__(self, ode_func=None):
+            super(Model, self).__init__()
+            if ode_func is None:
+                self._ode = ODEFunc().to(device)
+            else:
+                self._ode = ode_func
+            self.set_y0()
+            self.set_voltage_protocol_batches()
+            self.set_discontinous()
+
+        def n_parameters(self):
+            #return 9
+            return 4
+
+        def n_outputs(self):
+            if self._ps is not None:
+                return len(self._ps)
+            else:
+                return 1
+
+        def set_fixed_form_voltage_protocol(self, t, v):
+            self._ode.set_fixed_form_voltage_protocol(t, v)
+
+        def set_voltage_protocol_batches(self, ps=None):
+            "ps: list of voltage time series [times, voltages]"
+            self._ps = ps
+
+        def set_y0(self, y0=np.asarray([0, 1])):
+            self._y0 = y0.reshape(1, -1)
+            self._y0_torch = torch.from_numpy(self._y0).float()
+
+        def set_discontinous(self, discontn=None):
+            if discontn is not None:
+                self.discontn = torch.from_numpy(discontn)
+            else:
+                self.discontn = discontn
+
+        def simulate(self, x, t):
+            "Pints's forward simulation, x parameters, t time series"
+            t = torch.from_numpy(np.copy(t))
+            #g = x[0]
+            g = 1
+            #self._ode.set_parameters(x[1:])
+            self._ode.set_parameters(x[:])
+            if self._ps is not None:
+                out = []
+                for p in self._ps:
+                    self.set_fixed_form_voltage_protocol(p[:, 0], p[:, 1])
+                    try:
+                        with time_limit(600):
+                            o = odeint(self._ode, self._y0_torch, t, method='dopri5')
+                        out.append((g * o[:, 0, 0] * o[:, 0, 1] * (self._ode._v(t).to(device) + 86)).cpu().detach().numpy().reshape(-1))
+                    except TimeoutException as e:
+                        out.append(np.ones(t.shape) * np.inf)
+                return np.asarray(out).T
+            else:
                 try:
                     with time_limit(600):
-                        o = odeint(self._ode, self._y0_torch, t, method='dopri5')
-                    out.append((g * o[:, 0, 0] * o[:, 0, 1] * (self._ode._v(t).to(device) + 86)).cpu().detach().numpy().reshape(-1))
+                        o = odeint(self._ode, self._y0_torch, t, method='dopri5', options={"grid_points": self.discontn, "eps": 1e-6})
                 except TimeoutException as e:
-                    out.append(np.ones(t.shape) * np.inf)
-            return np.asarray(out).T
-        else:
-            try:
-                with time_limit(600):
-                    o = odeint(self._ode, self._y0_torch, t, method='dopri5', options={"grid_points": self.discontn, "eps": 1e-6})
-            except TimeoutException as e:
-                return np.ones(t.shape) * np.inf
-            return (g * o[:, 0, 0] * o[:, 0, 1] * (self._ode._v(t).to(device) + 86)).cpu().detach().numpy().reshape(-1)
+                    return np.ones(t.shape) * np.inf
+                return (g * o[:, 0, 0] * o[:, 0, 1] * (self._ode._v(t).to(device) + 86)).cpu().detach().numpy().reshape(-1)
+
 
 #
 #
 #
 if args.debug:
-    func = ODEFunc().to(device)
-    model = Model(func)
+    if args.myokit:
+        model = Model()
+    else:
+        func = ODEFunc().to(device)
+        model = Model(func)
     # model.set_fixed_form_voltage_protocol(prediction_protocol[:, 0], prediction_protocol[:, 1])
     model.set_fixed_form_voltage_protocol(time1, voltage1)
     o = model.simulate(p0, time1)
@@ -320,13 +435,15 @@ if __name__ == '__main__':
     datat = np.append(true_yo_batches1, true_yo_batches2)
     datat += np.random.normal(0, noise_sigma, datat.shape)
 
-    change_pt = np.append([True], ~(voltaget[1:] != voltaget[:-1]))
-    discontinuous_time = timet[~np.roll(change_pt, -1)]
-
-    func = ODEFunc().to(device)
-    model = Model(func)
+    if args.myokit:
+        model = Model()
+    else:
+        func = ODEFunc().to(device)
+        model = Model(func)
+        change_pt = np.append([True], ~(voltaget[1:] != voltaget[:-1]))
+        discontinuous_time = timet[~np.roll(change_pt, -1)]
+        model.set_discontinous(discontinuous_time)
     model.set_fixed_form_voltage_protocol(timet, voltaget)
-    model.set_discontinous(discontinuous_time)
 
     timet = timet[::10]
     datat = datat[::10]
@@ -336,6 +453,7 @@ if __name__ == '__main__':
     #problem = pints.MultiOutputProblem(model, timet, datat)
     error = pints.SumOfSquaresError(problem)
     print(error(p0))
+    transform = pints.LogTransformation(n_parameters=problem.n_parameters())
 
     #"""
     import time
@@ -358,6 +476,7 @@ if __name__ == '__main__':
         sigma0=p0 * 1e-1,
         boundaries=pints.RectangularBoundaries(p0 * 0.1, p0 * 10),
         method=pints.CMAES,
+        transform=transform,
     )
     opt.set_max_iterations(None)
     opt.set_max_unchanged_iterations(iterations=100, threshold=1e-3)
