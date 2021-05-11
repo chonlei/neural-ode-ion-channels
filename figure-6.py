ae245b026edce8adc2df6ffad3d0ee5fc24aec56
Chon Lok Lei <chonloklei@gmail.com>
2021-05-11 17:01:09 +0800

    Update figure 6

--- figure-6.py
+++ figure-6.py
@@ -40,8 +40,8 @@ true_y0s = [torch.tensor([[1., 0.]]).to(device),  # what you get after holding a
 
 # B1.2 in https://physoc.onlinelibrary.wiley.com/action/downloadSupplement?doi=10.1113%2FJP275733&file=tjp12905-sup-0001-textS1.pdf#page=4
 e = e_nnf = torch.tensor([-88.4]).to(device)  # assume we know
-# https://github.com/CardiacModelling/FourWaysOfFitting/blob/master/method-3/cell-5-fit-3-run-001.txt
-g = torch.tensor([0.133898199260611944]).to(device)  # assume we know
+# Table F11, Cell #5 GKr, in https://physoc.onlinelibrary.wiley.com/action/downloadSupplement?doi=10.1113%2FJP275733&file=tjp12905-sup-0001-textS1.pdf#page=20
+g = torch.tensor([0.1524]).to(device)  # assume we know
 
 g_nn = g * 1.2  # just because we see a-gate gets to ~1.2 at some point (in prt V=50), so can absorb that into the g.
 
@@ -77,6 +77,56 @@ currentp3 = prediction3[:, 1]
 voltagep3 = prediction3[:, 2]
 
 
+class Lambda(nn.Module):
+    def __init__(self):
+        super(Lambda, self).__init__()
+
+        # Fit to GroundTruth model using train-d0.py, in `./r0-bad/model-parameters.txt`.
+        self.p1 = 1.907386659660279861e-04
+        self.p2 = 4.884512388490065693e-02
+        self.p3 = 1.331886974861103986e-05
+        self.p4 = 6.220652138222864530e-02
+        # https://physoc.onlinelibrary.wiley.com/action/downloadSupplement?doi=10.1113%2FJP275733&file=tjp12905-sup-0001-textS1.pdf#page=20
+        # Table F11, Cell #5 parameters
+        self.p5 = 8.73e-2
+        self.p6 = 8.90e-3
+        self.p7 = 5.20e-3
+        self.p8 = 3.16e-2
+
+        self.unity = torch.tensor([1]).to(device)
+
+    def set_fixed_form_voltage_protocol(self, t, v):
+        # Regular time point voltage protocol time series
+        self._t_regular = t
+        self._v_regular = v
+        self.__v = interp1d(t, v)
+
+    def _v(self, t):
+        return torch.from_numpy(self.__v([t.cpu().numpy()])).to(device)
+
+    def voltage(self, t):
+        # Return voltage
+        return self._v(t).numpy()
+
+    def forward(self, t, y):
+        a, r = torch.unbind(y[0])
+
+        try:
+            v = self._v(t).to(device)
+        except ValueError:
+            v = torch.tensor([-80]).to(device)
+
+        k1 = self.p1 * torch.exp(self.p2 * v)
+        k2 = self.p3 * torch.exp(-self.p4 * v)
+        k3 = self.p5 * torch.exp(self.p6 * v)
+        k4 = self.p7 * torch.exp(-self.p8 * v)
+
+        dadt = k1 * (self.unity - a) - k2 * a
+        drdt = -k3 * r + k4 * (self.unity - r)
+
+        return torch.stack([dadt[0], drdt[0]])
+
+
 class ODEFunc1_6(nn.Module):
 
     def __init__(self):
@@ -106,11 +156,12 @@ class ODEFunc1_6(nn.Module):
         self.vrange = torch.tensor([100.]).to(device)
         self.netscale = torch.tensor([1000.]).to(device)
 
-        # https://github.com/CardiacModelling/FourWaysOfFitting/blob/master/method-3/cell-5-fit-3-run-001.txt
-        self.p5 = 8.73243709432939552e-02
-        self.p6 = 7.33380025549188515e-03
-        self.p7 = 6.16551007196145754e-03
-        self.p8 = 3.15741310933875322e-02
+        # https://physoc.onlinelibrary.wiley.com/action/downloadSupplement?doi=10.1113%2FJP275733&file=tjp12905-sup-0001-textS1.pdf#page=20
+        # Table F11, Cell #5 parameters
+        self.p5 = 8.73e-2
+        self.p6 = 8.90e-3
+        self.p7 = 5.20e-3
+        self.p8 = 3.16e-2
 
         self.unity = torch.tensor([1]).to(device)
 
@@ -153,6 +204,9 @@ class ODEFunc1_6(nn.Module):
 #
 #
 #
+func = Lambda().to(device)
+func.eval()
+
 func_1 = ODEFunc1_6().to(device)
 func_1.load_state_dict(torch.load('r1-bad/model-state-dict.pt'))
 func_1.eval()
@@ -170,6 +224,9 @@ def predict(func, time, voltage, time_torch, data, gg, y0, e, name):
 
 
 if args.cached:
+    pred_y_o_pr3 = torch.load('figure-6/y0-pr3.pt')
+    pred_y_o_pr4 = torch.load('figure-6/y0-pr4.pt')
+    pred_y_o_pr5 = torch.load('figure-6/y0-pr5.pt')
     pred_y_1_pr3 = torch.load('figure-6/y1-pr3.pt')
     pred_y_1_pr4 = torch.load('figure-6/y1-pr4.pt')
     pred_y_1_pr5 = torch.load('figure-6/y1-pr5.pt')
@@ -184,6 +241,7 @@ else:
         #
         # Trained Neural ODE
         makedirs('figure-6/pr3')
+        pred_y_o = predict(func, time1, voltage1, time1_torch, current1, g, true_y0, e, 'Pr3 (M0)')
         pred_y_1 = predict(func_1, time1, voltage1, time1_torch, current1, g_nn, true_y0, e_nnf, 'Pr3 (M1)')
 
         l = int(len(time1) / 7)  # 7 steps
@@ -221,9 +279,11 @@ else:
         plt.close(fig1)
 
         # Cache it
+        torch.save(pred_y_o, 'figure-6/y0-pr3.pt')
         torch.save(pred_y_1, 'figure-6/y1-pr3.pt')
+        pred_y_o_pr3 = pred_y_o
         pred_y_1_pr3 = pred_y_1
-        del(pred_y_1)
+        del(pred_y_o, pred_y_1)
 
 
         #
@@ -231,6 +291,7 @@ else:
         #
         # Trained Neural ODE
         makedirs('figure-6/pr4')
+        pred_y_o = predict(func, time2, voltage2, time2_torch, current2, g, true_y0, e, 'Pr4 (M0)')
         pred_y_1 = predict(func_1, time2, voltage2, time2_torch, current2, g_nn, true_y0, e_nnf, 'Pr4 (M1)')
 
         l = int(len(time2) / 16)  # 16 steps
@@ -268,9 +329,11 @@ else:
         plt.close(fig1)
 
         # Cache it
+        torch.save(pred_y_o, 'figure-6/y0-pr4.pt')
         torch.save(pred_y_1, 'figure-6/y1-pr4.pt')
+        pred_y_o_pr4 = pred_y_o
         pred_y_1_pr4 = pred_y_1
-        del(pred_y_1)
+        del(pred_y_o, pred_y_1)
 
 
         #
@@ -278,6 +341,7 @@ else:
         #
         makedirs('figure-6/pr5')
         # Trained Neural ODE
+        pred_y_o = predict(func, timep3, voltagep3, timep3_torch, currentp3, g, true_y0, e, 'Pr5 (M0)')
         pred_y_1 = predict(func_1, timep3, voltagep3, timep3_torch, currentp3, g_nn, true_y0, e_nnf, 'Pr5 (M1)')
 
         l = int(len(timep3) / 9)  # 9 steps
@@ -309,9 +373,11 @@ else:
         plt.close(fig1)
 
         # Cache it
+        torch.save(pred_y_o, 'figure-6/y0-pr5.pt')
         torch.save(pred_y_1, 'figure-6/y1-pr5.pt')
+        pred_y_o_pr5 = pred_y_o
         pred_y_1_pr5 = pred_y_1
-        del(pred_y_1)
+        del(pred_y_o, pred_y_1)
     #sys.exit()
 
 
@@ -368,8 +434,10 @@ for i in range(7)[::2]:
     axes[0, 0].plot(time1[:l], voltage1[l*i:l*(i+1)], c='#7f7f7f', ds='steps')
 
     axes[1, 0].plot(time1[:l:ds], current1.reshape(-1)[l*i:l*(i+1):ds], c='#7f7f7f', label='__nolegend__' if i else 'Data')
+    axes[1, 0].plot(time1[:l:ds], pred_y_o_pr3.reshape(-1).cpu().numpy()[l*i:l*(i+1):ds], '--', c='C0',
+                    label='__nolegend__' if i else r"'Improperly' trained candidate model")
     axes[1, 0].plot(time1[:l:ds], pred_y_1_pr3.reshape(-1).cpu().numpy()[l*i:l*(i+1):ds], '--', c='C3',
-                    label='__nolegend__' if i else r'Improperly trained $a$-gate as NN (NN-f)')
+                    label='__nolegend__' if i else r"'Improperly' trained $a$-gate as NN (NN-f)")
 
 axes[0, 0].set_xlim([0, 7500])
 axes[1, 0].set_xlim([0, 7500])
@@ -380,6 +448,7 @@ for i in range(16)[::2]:
     axes[0, 1].plot(time2[:l], voltage2[l*i:l*(i+1)], c='#7f7f7f', ds='steps')
 
     axes[1, 1].plot(time2[:l:ds], current2.reshape(-1)[l*i:l*(i+1):ds], c='#7f7f7f')
+    axes[1, 1].plot(time2[:l:ds], pred_y_o_pr4.reshape(-1).cpu().numpy()[l*i:l*(i+1):ds], '--', c='C0')
     axes[1, 1].plot(time2[:l:ds], pred_y_1_pr4.reshape(-1).cpu().numpy()[l*i:l*(i+1):ds], '--', c='C3')
 
 axes[0, 1].set_xlim([0, 2000])
@@ -391,6 +460,7 @@ for i in range(9)[::2]:
     axes[2, 0].plot(timep3[:l], voltagep3[l*i:l*(i+1)], c='#7f7f7f', ds='steps')
 
     axes[3, 0].plot(timep3[:l:ds], currentp3.reshape(-1)[l*i:l*(i+1):ds], c='#7f7f7f')
+    axes[3, 0].plot(timep3[:l:ds], pred_y_o_pr5.reshape(-1).cpu().numpy()[l*i:l*(i+1):ds], '--', c='C0')
     axes[3, 0].plot(timep3[:l:ds], pred_y_1_pr5.reshape(-1).cpu().numpy()[l*i:l*(i+1):ds], '--', c='C3')
 
 axes[2, 0].set_xlim([timep3[:l:ds][0], timep3[:l:ds][-1]])
@@ -402,7 +472,7 @@ axes[1, 1].set_ylim([-2.75, 11])
 axes[3, 0].set_ylim([-4.5, 5.5])
 
 axes[1, 0].legend(loc='lower left', bbox_to_anchor=(-0.02, 1.55), ncol=4,
-                  columnspacing=4, #handletextpad=1,
+                  columnspacing=3, #handletextpad=1,
                   bbox_transform=axes[1, 0].transAxes)
 
 axes[0, 0].text(-0.05, 1.05, '(A)', size=12, weight='bold',
